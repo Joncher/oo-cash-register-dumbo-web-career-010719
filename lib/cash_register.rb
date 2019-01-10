e6749bbb9e5e9bee4f5a8afb9a1a0a171cd3a80b
John Oncher <johnoncher@gmail.com>
2019-01-10 17:26:23 -0500

    used self

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,41 @@
+require 'pry'
+class CashRegister
 
+  attr_accessor :total, :discount, :items
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quanity = 1)
+    if quanity > 1
+      n = 1
+      while  n < quanity
+        @items << title
+        n += 1
+      end
+    end
+    @items << title
+    @total += price * quanity
+  end
+
+  def apply_discount
+
+    if @discount > 0
+      @total -= (@total * (@discount.to_f/100))
+      "After the discount, the total comes to $#{@total.to_i}."
+    else "There is no discount to apply."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    voided = @items.pop
+    @total -=self.total
+  end
+end
